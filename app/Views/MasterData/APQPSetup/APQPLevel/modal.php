97804e0ccac3bd983f353e9c069fe2b5f8a54b7d
ardy-priyo <priyo.ardy@hotmail.com>
2025-11-12 11:20:56 +0700

    Preparing to processing approver list

--- app/Views/MasterData/APQPSetup/APQPLevel/modal.php
+++ app/Views/MasterData/APQPSetup/APQPLevel/modal.php
@@ -3,21 +3,21 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">APQP Approver List</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="clearModel()"></button>
             </div>
             <div class="modal-body">
                 <div class="table-responsive">
                     <table class="table table-striped table-hover table-primary" id="tableApprover">
                         <thead>
-                            <th class="text-center align-middle bg-secondary-subtle col-1">Level</th>
-                            <th class="text-center align-middle bg-secondary-subtle col-5">Name</th>
-                            <th class="text-center align-middle bg-secondary-subtle col-6">Action</th>
+                            <th class="text-center align-middle bg-secondary-subtle col-8">Name</th>
+                            <th class="text-center align-middle bg-secondary-subtle col-4">Action</th>
                         </thead>
                         <tbody id="listApprover"></tbody>
                     </table>
                 </div>
             </div>
-            <div class=" modal-footer">
+            <div class="modal-footer justify-content-center">
+                <button type="button" class="btn btn-secondary rounded-0" data-bs-dismiss="modal" onclick="clearModel()"><i class="bi bi-x-circle"></i>&ensp;Close</button>
             </div>
         </div>
     </div>
--- public/js/MasterData/APQPSetup/APQPLevel/approver.js
+++ public/js/MasterData/APQPSetup/APQPLevel/approver.js
@@ -10,6 +10,7 @@ function showApprover(token) {
     )
       .then((result) => {
         if (result.data !== null) {
+          approvalList(result.data);
         } else {
           firstRow();
         }
@@ -25,11 +26,26 @@ function showApprover(token) {
   }
 }
 
+function approvalList(approver) {
+  const tBody = document.getElementById("listApprover");
+  tBody.innerHTML = "";
+  approver.forEach((item) => {
+    const row = `
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+    `;
+
+    tBody.insertAdjacentHTML("beforeend", row);
+  });
+}
+
 function firstRow() {
   const tableBody = document.getElementById("listApprover");
+  tableBody.innerHTML = "";
   const row = `
     <tr>
-        <td></td>
         <td>
             <select name="approver[]" class="form-control select2 select2bs5" required>
                 <option value="">-- Choose --</option>
@@ -37,6 +53,7 @@ function firstRow() {
         </td>
         <td>
             <button type="button" class="btn btn-sm btn-primary rounded-0" onclick="saveRow(this)"><i class="bi bi-floppy"></i></button>
+            <button type="button" class="btn btn-sm btn-success rounded-0" onclick="addRow()"><i class="bi bi-plus-circle"></i></button>
         </td>
     </tr>
   `;
@@ -49,4 +66,51 @@ function firstRow() {
     dropdownCssClass: "rounded-0",
     selectionCssClass: "rounded-0",
   });
+}
+
+function addRow() {
+  const tableBody = document.getElementById("listApprover");
+  const row = `
+    <tr>
+        <td>
+            <select name="approver[]" class="form-control select2 select2bs5" required>
+                <option value="">-- Choose --</option>
+            </select>
+        </td>
+        <td>
+            <button type="button" class="btn btn-sm btn-primary rounded-0" onclick="saveRow(this)"><i class="bi bi-floppy align-middle"></i></button>
+            <button type="button" class="btn btn-sm btn-success rounded-0" onclick="addRow()"><i class="bi bi-plus-circle align-middle"></i></button>
+            <button type="button" class="btn btn-sm btn-danger rounded-0" onclick="removeRow(this)"><i class="bi bi-dash-circle align-middle"></i></button>
+        </td>
+    </tr>
+  `;
+
+  tableBody.insertAdjacentHTML("beforeend", row);
+
+  $(".select2bs5").select2({
+    dropdownParent: $("#modalApprover"),
+    theme: "bootstrap-5",
+    dropdownCssClass: "rounded-0",
+    selectionCssClass: "rounded-0",
+  });
+}
+
+function clearModel() {
+  const tableBody = document.getElementById("listApprover");
+  tableBody.innerHTML = "";
+}
+
+function removeRow(btn) {
+  const row = btn.closest("tr");
+
+  if (row) {
+    const tbody = document.getElementById("listApprover");
+    row.remove();
+  }
+}
+
+function saveRow(btn) {
+  const row = btn.closest("tr");
+  const token = document.getElementById("modal_token").value;
+  const approver = row.querySelector('select[name="approver"]').value;
 }
